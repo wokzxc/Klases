24887c20da8caa34cb054a2d599784f949a85efc
wokzxc <100773702+wokzxc@users.noreply.github.com>
2022-03-07 10:48:03 +0200

    Update main.cpp Ilmārs
    feat Kirills

--- main.cpp
+++ main.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
 using namespace std;
 
-class twointegers
-{
-public:
+class twointegers(int a, int b) {
+private:
     int a;
     int b;
+public:
+int sum=a+b;
 };
-
 int main()
 {
-    twointegers t;
-    t.a = 5;
-    t.b = 7;
-    cout << t.a+t.b << endl;
+    twointegers t(5,7);
+    cout << t.sum << endl;
     return 0;
 }
 
-/*
-int main()
-{
-    twointegers t (5, 7);
-    cout << t.sum() << endl;
-    return 0;
-}
-*/
+
+
